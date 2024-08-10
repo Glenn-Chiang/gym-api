692a2821efb5c231b0e33baf0a34dfcbe7946bbe
glenn <glennchiang24@gmail.com>
2024-08-10 13:46:46 +0800

    Set default config values

--- config.ts
+++ config.ts
@@ -1,16 +1,20 @@
 import dotenv from "dotenv";
-dotenv.config()
+
+// In development, read environment variables from .env file
+if (process.env.NODE_ENV === "development") {
+  dotenv.config() 
+}
 
 export const config = {
-  nodeEnv: process.env.NODE_ENV ?? "development",
-  port: process.env.PORT,
+  nodeEnv: process.env.NODE_ENV || "development",
+  port: process.env.PORT || 3000,
 
   dbConfig: {
-    user: process.env.PGUSER!,
-    password: process.env.PGPASSWORD!,
-    host: process.env.PGHOST!,
-    port: Number(process.env.PGPORT!),
-    database: process.env.PGDATABASE!,
+    user: process.env.PGUSER || "postgres",
+    password: process.env.PGPASSWORD || "password",
+    host: process.env.PGHOST || "localhost",
+    port: Number(process.env.PGPORT) || 3000,
+    database: process.env.PGDATABASE || "postgres",
     ssl: false
   }
 };
--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,17 +1,21 @@
-import { defineConfig } from "drizzle-kit"
+import { defineConfig } from "drizzle-kit";
 import dotenv from "dotenv";
-dotenv.config()
+
+// In development, read environment variables from .env file
+if (process.env.NODE_ENV === "development") {
+  dotenv.config();
+}
 
 export default defineConfig({
-  schema: './src/db/schema/*',
+  schema: "./src/db/schema/*",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
-    user: process.env.PGUSER!,
-    password: process.env.PGPASSWORD!,
-    host: process.env.PGHOST!,
-    port: Number(process.env.PGPORT!),
-    database: process.env.PGDATABASE!,
-    ssl: false
-  }
-})
+    user: process.env.PGUSER || "postgres",
+    password: process.env.PGPASSWORD || "password",
+    host: process.env.PGHOST || "localhost",
+    port: Number(process.env.PGPORT) || 3000,
+    database: process.env.PGDATABASE || "postgres",
+    ssl: false,
+  },
+});
